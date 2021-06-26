1135afdc2547a86780a8bc573e856c174760bb71
Jessica Stern <jlstern3@gmail.com>
2021-06-26 09:49:37 -0400

    All Authors displayed, paths calling properly

---
+++ authors_fullstack/client/src/components/AllAuthors.js
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {Link,navigate} from '@reach/router';
+import AuthorForm from './AuthorForm';
+
+const AllAuthors = (props) => {
+    //need state to hold an array to map through 
+    const [authors, setAuthors] = useState([])
+
+    useEffect(()=> {
+        axios.get("http://localhost:8000/api/authors")
+            .then((res) => {
+                console.log(res.data);
+                setAuthors(res.data)
+            })
+            .catch((err)=> {
+                console.log(err);
+            })
+    }, []);
+
+    return(
+        <div>
+            <Link to = "/api/authors/new"><button>Add an author</button></Link>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Author</td>
+                        <td>Actions Available</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                    {
+                        authors.map((author, index)=> (
+                        <tr>
+                        <td key = {index}>{author.name}</td>
+                        <td><Link to = {"/api/authors/" + author._id + "/edit"}><button>Edit</button></Link><button>Delete</button></td>
+                        </tr>
+                        ))
+                    }
+
+                        
+                    
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default AllAuthors;

f6a27a56e9fe9b7f78ad8cf098f2150df116fe03
Jessica Stern <jlstern3@gmail.com>
2021-06-27 22:03:53 -0400

    Mongoose successfully connect to db

--- team_manager/server/server.js
+++ team_manager/server/server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//mongoose
-// require('./config/mongoose.config');
+
+require('./config/mongoose.config');
 
 // connect routes here
 
